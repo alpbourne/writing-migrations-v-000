c3d1d844d21381bf77058c534e25cf0bdcf44fd7
alpbourne <alpbourne@gmail.com>
2017-11-01 19:21:03 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class AddColumns < ActiveRecord::Migration
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration
   def change
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
